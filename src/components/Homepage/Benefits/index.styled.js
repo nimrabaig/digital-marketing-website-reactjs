d489531472cef56dc02dd96f8cfeb90218a6304b
nimrabaig <nimrabaig28@gmail.com>
2023-11-10 23:20:01 +0500

    added benefits background image

--- src/components/Homepage/Benefits/index.styled.js
+++ src/components/Homepage/Benefits/index.styled.js
@@ -1,5 +1,5 @@
 import { styled } from "styled-components";
-import BenefitsBg from "../../../assets/benefits-bg.png";
+import BenefitsBg from "../../../assets/benefits-bg.jpg";
 
 export const Wrapper = styled.div`
   width: 100%;
